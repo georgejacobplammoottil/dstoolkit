1fca0e5af18beb90256cb02210effb171539292a
George Jacob Plammoottil <96573918+georgejacobplammoottil@users.noreply.github.com>
2023-07-18 22:59:58 +0530

    Update dstoolkit.py
    Update binner to handle string based categorical variables

--- dstoolkit.py
+++ dstoolkit.py
@@ -3,6 +3,9 @@
 # Imports
 import pandas as pd
 import numpy as np
+from pandas.api.types import is_string_dtype
+pd.options.mode.chained_assignment = None  # default='warn'
+pd.set_option('display.max_rows', 100)
 
 # Test file
 ds = pd.read_csv("/Users/georgeplammoottil/Documents/Projects/Stat101/ds_salaries.csv")
@@ -49,7 +52,6 @@ means(ds,'salary',1,2)
 means(ds, 'remote_ratio',1,2)
 
 
-
 # 2. Function that creates bins of a variable for bivariate analysis
 # Allow user to specify number of bins
 # Check for distinct values available for binning
@@ -65,41 +67,46 @@ def get_bucket(df, x_column, y_column, ):
         Y_avg = (y_column, 'mean'))
     
     df1['cumsum'] = df1['Observations'].cumsum()
-
     return df1
 
 def bucket_label(df, x_column, no_of_bins):
 
     df = df.sort_values(by=x_column,ascending=True, na_position='first')
     totalobs = df['cumsum'].max()
-    bucket_size = totalobs/no_of_bins
     df['bin'] = no_of_bins-1
     cum_bucket_last = df['cumsum'].min()
 
     for bin in range(no_of_bins):
         cum_bucket_max = (bin+1)*(totalobs/no_of_bins)
-        df.loc[(cum_bucket_last<df['cumsum']) & (df['cumsum']<cum_bucket_max), 'bin'] = bin
+        df.loc[(cum_bucket_last<=df['cumsum']) & (df['cumsum']<cum_bucket_max), 'bin'] = bin
         cum_bucket_last = df.loc[df['bin'] == bin, 'cumsum'].max()
     return df
 
-
 def binner(ds, y_column, x_column, no_of_bins, exclusions):
+    
     print("\n**************************************************\n")
     print(f"*** Bivariate table - {x_column} and {y_column} ****\n")
     # Keep relevant columns
     bin_ds = ds[[x_column, y_column]]
+    distinct_values = bin_ds[x_column].nunique()
+
+    if bin_ds[x_column].dtype.kind not in 'biufc':
+        bin_ds[x_column] = bin_ds[x_column].astype(str)
+        xisstring=1
+    else:
+        xisstring=0
 
     # Apply exclusions for X
     for exclusion in exclusions:
         bin_ds = bin_ds[bin_ds[x_column] != exclusion]
-
+        
     # Sort and Group rows based on value
-    bin_ds_sorted = bin_ds.sort_values(by=x_column,ascending=True, na_position='first')
-    bin_ds_distinct = get_bucket(bin_ds_sorted, x_column, y_column)
+    if xisstring==0:
+        bin_ds_sorted = bin_ds.sort_values(by=x_column,ascending=True, na_position='first')
+        bin_ds_distinct = get_bucket(bin_ds_sorted, x_column, y_column)
     
-    # Handling repeated values - Check if distinct values < number of bins specified
-    distinct_values = bin_ds[x_column].nunique()
-    if distinct_values < no_of_bins:
+    # Handling repeated values - Check if distinct values < number of bins specified 
+    if xisstring==0 and distinct_values < no_of_bins:
         print(f" {distinct_values} unique values available to create {no_of_bins} bins")
         no_of_bins = distinct_values
         res = bin_ds_distinct.groupby(x_column).aggregate(
@@ -110,17 +117,41 @@ def binner(ds, y_column, x_column, no_of_bins, exclusions):
             yavg = ('Y_avg', 'mean'))
         res['cumsum'] = res['Observations'].cumsum()
 
-    else: 
-        # Handling repeated values - if distinct values>no of bins, roll up to distinct values
+    elif xisstring==0: 
+        # Handling repeated values - if distinct values>no of bins, roll up to distinct values and bin
         bin_ds_bucket = bucket_label(bin_ds_distinct, x_column, no_of_bins)
         
         res = bin_ds_bucket.groupby('bin').aggregate(
-            Observations=('Observations', 'sum'), 
+            observations=('Observations', 'sum'), 
             min=('X_min','min'), 
             max=('X_max', 'max'),
             avg = ('X_avg','mean'), 
             yavg = ('Y_avg', 'mean'))
+        res['cumsum'] = res['observations'].cumsum()
+    
+    if xisstring==0:
+        res.rename(columns = {
+            'min': x_column+'_min', 
+            'max': x_column+'_max',
+            'avg': x_column+'_mean', 
+            'yavg': y_column+'_mean',
+            'cumsum': 'cummulative count'}, 
+            inplace = True)
+    else:
+        # Handling categorical string variables
+        res = bin_ds.groupby(x_column).aggregate(
+            Observations=(x_column, 'count'), 
+                yavg = (y_column, 'mean')).sort_values(
+            by='Observations',
+            ascending=False, 
+            na_position='first')
+        
         res['cumsum'] = res['Observations'].cumsum()
+        
+        res.rename(columns = { 
+            'yavg': y_column+'_mean',
+            'cumsum': 'cummulative count'}, 
+            inplace = True)
     
     print(res)
     print("\n**************************************************\n")
@@ -128,5 +159,6 @@ def binner(ds, y_column, x_column, no_of_bins, exclusions):
 
 binner(ds, 'remote_ratio','salary',10,[])
 binner(ds, 'salary','remote_ratio',10,[])
+binner(ds, 'remote_ratio','job_title',10,[])
 
 
